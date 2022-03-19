1c2cac69ba0a2ccaffea27b271ead2e20593219d
malaheaven <yunhg95@gmail.com>
2022-03-19 21:27:04 +0900

    [ADD] h2 db 설정
    - querydsl 변경
    - h2 적용
    - 로그 단계에서 확인하기 위해 p6spy 적용(운영에서는 사용하지 않는걸 권장)

--- querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -7,12 +7,14 @@ import com.study.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
     @Autowired
